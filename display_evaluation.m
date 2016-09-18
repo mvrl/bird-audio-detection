f22aefb5736d48ea47257df91e366de2ba8b9ae6
Nathan Jacobs <jacobs@cs.uky.edu>
2016-09-18 08:05:18 -0400

    initial step toward future stratified plots

--- display_evaluation.m
+++ display_evaluation.m
@@ -4,33 +4,33 @@ addpath ~/matlab_root/
 runs = {'elu_piezo', 'elu_eeg', 'relu_piezo', 'relu_eeg', 'elu_piezo_2', 'elu_eeg_2', 'relu_piezo_2', 'relu_eeg_2'};
 names = {'WAKE','NREM','REM'};
 
-eval = cell(numel(runs),numel(names));
+% results across all datasets
+eval_all = cell(numel(runs),numel(names));
 
 for irun = 1:numel(runs)
   
   run = runs{irun};
   log = @(s) fprintf('%s: %s\n',run,s);
-
+  
   
   % WARNING: only using a subset of the testing data
   log('loading data')
-  data = dlmread(['checkpoint/' run '/output.csv'],' ',[0,0,200000,7]);
-  
-  good = data(:,end-1) == data(:,end);
-  fileids = data(good,1)
-  rows = data(good,2)
-  scores = data(good,3:end-2);
-  labels = data(good,end);
+  data = dlmread(['checkpoint/' run '/output.csv'],' ',[0,0,200000,6]);
   
   %% make 1-vs-all ROC curves
   
+  good = data(:,end-1) == data(:,end);
+  fileids = data(good,1);
+  rows = data(good,2);
+  scores = data(good,3:end-2);
+  labels = data(good,end);
   
   for iname = 1:numel(names)
     
     log('computing perfcurve')
     [tmp.X,tmp.Y,tmp.T,tmp.AUC] = perfcurve(labels,scores(:,iname),iname);
     
-    eval{irun,iname} = tmp;
+    eval_all{irun,iname} = tmp;
     
   end
   
@@ -43,7 +43,7 @@ figure(1);clf
 for iname = 1:numel(names)
   subplot(1,3,iname)
   for irun = 1:numel(runs)
-    tmp = eval{irun,iname};
+    tmp = eval_all{irun,iname};
     plot(tmp.X,tmp.Y)
     hold on
   end
@@ -51,7 +51,7 @@ for iname = 1:numel(names)
   grid on
   legend_text = cellfun(...
     @(name,eval) sprintf('%s (%02.4f)',name,eval.AUC), ...
-    runs, eval(:,iname)','UniformOutput', false);
+    runs, eval_all(:,iname)','UniformOutput', false);
   legend(legend_text,'Interpreter','none','Location','southeast')
   set(gca,'XTick', linspace(0,1,5))
   set(gca,'YTick', linspace(0,1,5))
