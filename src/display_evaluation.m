5fe0af4f2ee63812f6bb1990711089ab889e3070
Nathan Jacobs <jacobs@cs.uky.edu>
2016-11-28 14:49:23 -0500

    analysis fixes

--- src/display_evaluation.m
+++ src/display_evaluation.m
@@ -1,12 +1,11 @@
-addpath ~/matlab_root/
+clear
 
 %% load data
-runs = dir('checkpoint/*eeg*'); 
+runs = dir('../checkpoint/*_*');
 runs = {runs.name};
-names = {'WAKE','NREM','REM'};
 
 % results across all datasets
-eval_all = cell(numel(runs),numel(names));
+eval_all = cell(numel(runs),1);
 
 for irun = 1:numel(runs)
   
@@ -16,62 +15,49 @@ for irun = 1:numel(runs)
   
   % WARNING: only using a subset of the testing data
   log('loading data')
-  data = dlmread(['checkpoint/' run '/output.csv'],' ',[0,0,200000,6]);
+  data = dlmread(['checkpoint/' run '/output.csv'],' ',[0,0,1550,2]);
   
   %% make 1-vs-all ROC curves
   
-  good = data(:,end-1) == data(:,end);
-  fileids = data(good,1);
-  rows = data(good,2);
-  scores = data(good,3:end-2);
-  labels = data(good,end);
+  labels = data(:,1);
+  scores = data(:,2:end);
   
-  for iname = 1:numel(names)
-
-    tmp = struct('run',run);
-    
-    log('computing perfcurve')
-    [tmp.X,tmp.Y,tmp.T,tmp.AUC] = perfcurve(labels,scores(:,iname),iname);
-    
-    eval_all{irun,iname} = tmp;
-    
-  end
+  tmp = struct('run',run);
+  
+  log('computing perfcurve')
+  [tmp.X,tmp.Y,tmp.T,tmp.AUC] = perfcurve(labels,scores(:,2),1);
+  
+  eval_all{irun} = tmp;
   
 end
 
 %%
-
-load('performance.mat')
 
 % sort methods by min AUC
 [~,idx] = sort(min(cellfun(@(eval) eval.AUC,eval_all),[],2),'descend');
 
 figure(1);clf
 
-for iname = 1:numel(names)
-  subplot(1,3,iname)
-  for irun = 1:numel(runs)
-    tmp = eval_all{idx(irun),iname};
-    plot(tmp.X,tmp.Y)
-    hold on
-  end
-  axis square
-  grid on
-  legend_text = cellfun(...
-    @(name,eval) sprintf('%s (%02.4f)',name,eval.AUC), ...
-    runs, eval_all(idx,iname)','UniformOutput', false);
-  legend(legend_text,'Interpreter','none','Location','southeast')
-  set(gca,'XTick', linspace(0,1,5))
-  set(gca,'YTick', linspace(0,1,5))
-  xlabel('False positive rate')
-  ylabel('True positive rate')
-  title(['ROC (' names{iname} ')'])
-  
+for irun = 1:numel(runs)
+  tmp = eval_all{idx(irun)};
+  plot(tmp.X,tmp.Y)
+  hold on
 end
+axis square
+grid on
+legend_text = cellfun(...
+  @(name,eval) sprintf('%s (%02.4f)',name,eval.AUC), ...
+  runs, eval_all(idx)','UniformOutput', false);
+legend(legend_text,'Interpreter','none','Location','southeast')
+set(gca,'XTick', linspace(0,1,5))
+set(gca,'YTick', linspace(0,1,5))
+xlabel('False positive rate')
+ylabel('True positive rate')
+title('ROC')
 
 disp('exporting figure')
 exportfigure(gcf,'roc.pdf',[12,5.5])
 savefig(gcf,'roc.fig')
 
-save('performance.mat','runs','names','eval_all')
+save('performance.mat','runs','eval_all')
 
