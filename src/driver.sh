9fcce20b12f41c16e0e6518aaea0776b9937034a
Nathan Jacobs <jacobs@cs.uky.edu>
2016-11-28 14:36:43 -0500

    run evaluation on four networks

--- src/driver.sh
+++ src/driver.sh
@@ -2,47 +2,10 @@
 
 set -e
 
-. setup_gpu.sh
+. setup.sh
 
-python main.py -f eeg -a elu -c 1
-python main.py -f eeg -a relu -c 1 
-python main.py -f eeg -a lrelu -c 1 
-python main.py -f piezo -a elu -c 1 
-python main.py -f piezo -a relu -c 1 
-python main.py -f piezo -a lrelu -c 1 
-
-python evaluate.py -f eeg -a elu -c 1
-python evaluate.py -f eeg -a relu -c 1 
-python evaluate.py -f eeg -a lrelu -c 1 
-python evaluate.py -f piezo -a elu -c 1 
-python evaluate.py -f piezo -a relu -c 1 
-python evaluate.py -f piezo -a lrelu -c 1 
-
-python main.py -f eeg -a elu -c 1.2
-python main.py -f eeg -a relu -c 1.2
-python main.py -f eeg -a lrelu -c 1.2
-python main.py -f piezo -a elu -c 1.2
-python main.py -f piezo -a relu -c 1.2
-python main.py -f piezo -a lrelu -c 1.2
-
-python evaluate.py -f eeg -a elu -c 1.2
-python evaluate.py -f eeg -a relu -c 1.2
-python evaluate.py -f eeg -a lrelu -c 1.2
-python evaluate.py -f piezo -a elu -c 1.2
-python evaluate.py -f piezo -a relu -c 1.2
-python evaluate.py -f piezo -a lrelu -c 1.2
-
-python main.py -f eeg -a elu -c .8
-python main.py -f eeg -a relu -c .8 
-python main.py -f eeg -a lrelu -c .8 
-python main.py -f piezo -a elu -c .8 
-python main.py -f piezo -a relu -c .8 
-python main.py -f piezo -a lrelu -c .8 
-
-python evaluate.py -f eeg -a elu -c .8
-python evaluate.py -f eeg -a relu -c .8 
-python evaluate.py -f eeg -a lrelu -c .8 
-python evaluate.py -f piezo -a elu -c .8 
-python evaluate.py -f piezo -a relu -c .8 
-python evaluate.py -f piezo -a lrelu -c .8 
+python evaluate.py -n v2 -a relu -c 1
+python evaluate.py -n v2.1 -a relu -c 1
+python evaluate.py -n v2.2 -a relu -c 1
+python evaluate.py -n v3 -a relu -c 1
 
