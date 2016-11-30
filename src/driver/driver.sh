085a676de5e2a84c3b3a1587608da72fbc3b33dd
Nathan Jacobs <jacobs@cs.uky.edu>
2016-11-30 17:26:59 -0500

    update driver script to use GNU parallel

---
+++ src/driver/driver.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+pushd ../
+
+set -e
+
+NGPU=2
+
+. setup_gpu.sh
+
+rm -rf checkpoint/*/output.csv
+
+# run multiple jobs in parallel, but don't run more than one job on a
+# single GPU
+parallel -P ${NGPU} --line-buffer --rpl '{gpuid} $_ = $job->slot() - 1' \
+  CUDA_VISIBLE_DEVICES={gpuid} \
+  ./train_test.sh {} :::: methods.list
+
---
+++ src/driver/train_test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python train.py $1 
+python evaluate.py $1 
+
