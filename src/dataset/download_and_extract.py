7ece85250d996d3577627cc0455b96e64e37c931
Weilian Song <weiliansong@hotmail.com>
2016-12-02 16:12:17 -0500

    Removed url prefix usage

--- src/dataset/download_and_extract.py
+++ src/dataset/download_and_extract.py
@@ -2,17 +2,14 @@ import os
 import zipfile
 import shutil
 import requests
-from collections import defaultdict
 
-label_base = 'https://ndownloader.figshare.com/files/'
-audio_base = 'https://archive.org/download/'
 data_base  = '../../data/'
 
 datasets = {
-    'freefield1010' : { 'labels' : '6035814',
-                        'audio'  : 'ff1010bird/ff1010bird_wav.zip' },
-    'warblr'        : { 'labels' : '6035817',
-                        'audio'  : 'warblrb10k_public/warblrb10k_public_wav.zip' }
+    'freefield1010' : { 'labels' : 'https://ndownloader.figshare.com/files/6035814',
+                        'audio'  : 'https://archive.org/download/ff1010bird/ff1010bird_wav.zip' },
+    'warblr'        : { 'labels' : 'https://ndownloader.figshare.com/files/6035817',
+                        'audio'  : 'https://archive.org/download/warblrb10k_public/warblrb10k_public_wav.zip' }
 }
 
 def unzip_file(source, dest):
@@ -41,21 +38,16 @@ def get_file(url, f_name):
 
 if not os.path.exists(data_base):
     os.mkdir(data_base)
-                
-if not os.path.exists(data_base + 'ff1010_audio/'):
-    os.mkdir(data_base + 'ff1010_audio/')
-
-if not os.path.exists(data_base + 'warblr_audio/'):
-    os.mkdir(data_base + 'warblr_audio/')
 
 for key in datasets:
+    if not os.path.exists(data_base + '%s_audio' % key):
+        os.mkdir(data_base + '%s_audio' % key)
+
     print('Downloading %s labels' % key)
-    get_file(label_base + datasets[key]['labels'], 
-             data_base + '%s_labels.csv' % key)
+    get_file(datasets[key]['labels'], data_base + '%s_labels.csv' % key)
 
     print('Downloading %s audio' % key)
-    get_file(audio_base + datasets[key]['audio'], 
-             data_base + '%s_audio.zip' % key)
+    get_file(datasets[key]['audio'], data_base + '%s_audio.zip' % key)
 
     print('Unzipping %s audio' % key)
     unzip_file(data_base + '%s_audio.zip' % key, data_base + '%s_audio/' % key)
