84c1ef853ad7fc1393b89b8dd5d156a13013ae6e
Weilian Song <weiliansong@hotmail.com>
2016-12-22 00:20:10 -0500

    Different method for visualizing conv5

--- src/visualize.py
+++ src/visualize.py
@@ -4,6 +4,9 @@ import dataset
 import network
 import tensorflow as tf
 import matplotlib.pyplot as plt
+from scipy.misc import imresize
+from scipy.ndimage.filters import gaussian_filter
+
 slim = tf.contrib.slim
 
 ckpt_location = '/u/eag-d1/scratch/jacobs/birddetection/checkpoints/v2.1_elu_0.20_1.00_no'
@@ -66,19 +69,19 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
     for idx in range(20):
         sample = output['conv5'][idx]
 
-        plt.figure(1)
-        plt.subplot(311)
-        plt.plot(range(sample.shape[0]), sample[:,0,0])
-        plt.axis((0, sample.shape[0], -1.0, 1.0))
+        sample = sample.reshape((sample.shape[0], 26)).T
+        sample = imresize(sample, (26,150))
+        sample = gaussian_filter(sample, 1)
 
+        plt.figure(1)
+        plt.subplot(211)
+        plt.imshow(sample)
+        
         plt.title('%s %d %d' % (_recname[idx], _label[idx], _prediction[idx]))
 
-        plt.subplot(312)
-        plt.plot(range(sample.shape[0]), sample[:,0,1])
-        plt.axis((0, sample.shape[0], -1.0, 1.0))
-
-        plt.subplot(313)
+        plt.subplot(212)
         plt.plot(_feat[idx])
+        plt.axis((0, _feat[idx].size, -1.0, 1.0))
 
         plt.savefig('./conv5/test_%d.png' % idx)
         plt.clf()
